f3b74114a61b1d3377a3dfa994139ea527c57896
Amokrane Chentir <amokrane.chentir@gmail.com>
2011-06-03 01:55:24 +0200

    Updated readme file and added GitHub as a provider

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
 	provider :twitter, 'PFK5aQ7WM5KrF9PEPiByRg', 'whGdAmyMSyvQgmzLQzHoTQRG8KJkAf6cgbcSIm1pTJw'
+	provider :GitHub, '6f4f4b3ec23f177220fc', 'b20ec7e9976a509a7556188a7be9ab05704e914a'
 end
