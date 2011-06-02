63264900cbbb931fcbc04c1b7a960617250f523a
Amokrane Chentir <amokrane.chentir@gmail.com>
2011-06-03 01:16:34 +0200

    Removed the necessity to provide the current_password when editing users data

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -4,6 +4,18 @@ class RegistrationsController < Devise::RegistrationsController
 		session[:omniauth] = nil unless @user.new_record?
 	end
 
+	def update
+    	if resource.update_attributes(params[resource_name])
+      		set_flash_message :notice, :updated
+      		# Line below required if using Devise >= 1.2.0
+      		sign_in resource_name, resource, :bypass => true
+      		redirect_to after_update_path_for(resource)
+    	else
+      		clean_up_passwords(resource)
+      		render_with_scope :edit
+    	end
+	end
+
 	private
 	def build_resource(*args)
 		super
