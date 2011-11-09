645002853f93145163270b998f468e0b57d2a289
Amokrane Chentir <amokrane@submate.com>
2011-11-10 00:31:59 +0100

    adding archive content

--- db/seeds.rb
+++ db/seeds.rb
@@ -27,7 +27,23 @@ seed_codes_hash = [{"title" => "HelloWorld_C", "language" => "C", "content" => "
 	{"title" => "HelloWorld_C#", "language" => "C#", "content" => "```ruby\r\nputs \"Hello World!\"\r\n```", 
 	"description" => "Hello World In C#", "user_id" => User.find_by_email("chentir@poste.isima.fr").id},
 {"title" => "HelloWorld_ObjC", "language" => "ObjC", "content" => "```ruby\r\nputs \"Hello World!\"\r\n```", 
-	"description" => "Hello World In Objective-C", "user_id" => User.find_by_email("me@ghislaineguerin.com").id}]
+	"description" => "Hello World In Objective-C", "user_id" => User.find_by_email("me@ghislaineguerin.com").id},
+{"title" => "HelloWorld_Scala", "language" => "Scala", "content" => "```ruby\r\nputs \"Hello World!\"\r\n```", 
+	"description" => "Hello World In Scala", "user_id" => User.find_by_email("me@ghislaineguerin.com").id, "code_state_id" => "3", "score" => "299"},
+{"title" => "HelloWorld_Clojure", "language" => "Clojure", "content" => "```ruby\r\nputs \"Hello World!\"\r\n```", 
+	"description" => "Hello World In Clojure", "user_id" => User.find_by_email("me@ghislaineguerin.com").id, "code_state_id" => "3", "score" => "120"},
+{"title" => "HelloWorld_Nodejs", "language" => "NodeJS", "content" => "```ruby\r\nputs \"Hello World!\"\r\n```", 
+	"description" => "Hello World In NodeJs", "user_id" => User.find_by_email("me@ghislaineguerin.com").id, "code_state_id" => "3", "score" => "120"},
+{"title" => "HelloWorld_Lisp", "language" => "Lisp", "content" => "```ruby\r\nputs \"Hello World!\"\r\n```", 
+	"description" => "Hello World In Lisp", "user_id" => User.find_by_email("me@ghislaineguerin.com").id, "code_state_id" => "3", "score" => "44"},
+{"title" => "HelloWorld_F#", "language" => "F#", "content" => "```ruby\r\nputs \"Hello World!\"\r\n```", 
+	"description" => "Hello World In F#", "user_id" => User.find_by_email("me@ghislaineguerin.com").id, "code_state_id" => "3", "score" => "244"},
+{"title" => "HelloWorld_C#", "language" => "C#", "content" => "```ruby\r\nputs \"Hello World!\"\r\n```", 
+	"description" => "Hello World In C#", "user_id" => User.find_by_email("me@ghislaineguerin.com").id, "code_state_id" => "3", "score" => "515"},
+{"title" => "HelloWorld_C++", "language" => "C++", "content" => "```ruby\r\nputs \"Hello World!\"\r\n```", 
+	"description" => "Hello World In C++", "user_id" => User.find_by_email("me@ghislaineguerin.com").id, "code_state_id" => "3", "score" => "22"},
+{"title" => "HelloWorld_Lua", "language" => "Lua", "content" => "```ruby\r\nputs \"Hello World!\"\r\n```", 
+	"description" => "Hello World In Lua", "user_id" => User.find_by_email("me@ghislaineguerin.com").id, "code_state_id" => "3", "score" => "99"}]
 
 
 seed_codes_hash.each do |attributes|
